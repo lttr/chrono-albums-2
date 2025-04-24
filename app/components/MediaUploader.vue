4176309919a82bc123da9e2fc177c7801b3d4016
Lukas Trumm <lukas.trumm@gmail.com>
2025-04-24 14:48:43 +0200

    Add file listing

--- app/components/MediaUploader.vue
+++ app/components/MediaUploader.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <label class="upload-label" :for="fileUploadId">Upload files</label>
-    <input :id="fileUploadId" class="upload-input" type="file" multiple />
+    <section>
+      <label class="upload-label" :for="fileUploadId">Upload files</label>
+      <input
+        :id="fileUploadId"
+        class="upload-input"
+        type="file"
+        multiple
+        @change="handleFileSelect"
+      />
+    </section>
+    <section>
+      <p v-for="file of files" :key="file.name">{{ file.name }}</p>
+    </section>
   </div>
 </template>
 
 <script lang="ts" setup>
+const files = ref<File[]>([])
+
+function handleFileSelect(event: Event) {
+  const target = event.target as HTMLInputElement
+  if (target.files) {
+    files.value = files.value.concat(Array.from(target.files))
+  }
+}
+
 const fileUploadId = useId()
 </script>
 
