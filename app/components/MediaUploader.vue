bf30b4d016deb9f7d642fd02b6a697fd926d2b9d
Lukas Trumm <lukas.trumm@gmail.com>
2025-04-24 20:01:33 +0200

    File validations and previews

--- app/components/MediaUploader.vue
+++ app/components/MediaUploader.vue
@@ -7,16 +7,38 @@
         class="upload-input"
         type="file"
         multiple
+        :accept="acceptedFileTypes"
         @change="handleFileSelect"
       />
     </section>
     <section>
-      <p v-for="file of files" :key="file.name">{{ file.name }}</p>
+      <div v-for="(file, index) of files" :key="file.name">
+        {{ file.name }}
+        <div class="preview">
+          <img
+            v-if="previews[index]?.type === 'image'"
+            :src="previews[index].url"
+            alt=""
+          />
+          <video v-if="previews[index]?.type === 'video'" controls>
+            <source :src="previews[index].url" />
+          </video>
+        </div>
+        <span v-if="fileIsTooBig(file)">(too big)</span>
+      </div>
     </section>
   </div>
 </template>
 
 <script lang="ts" setup>
+const allowedMimeTypes = ["image/jpeg", "image/heif", "video/mp4", "video/mov"]
+const acceptedFileTypes = allowedMimeTypes
+  .concat([".jpeg", ".jpg", ".heic", ".mov", ".mp4"])
+  .join(",")
+
+const MAX_VIDEO_SIZE = 1024 * 1024 * 10 // 10 MB
+const MAX_IMAGE_SIZE = 1024 * 1024 * 100 // 200 MB
+
 const files = ref<File[]>([])
 
 function handleFileSelect(event: Event) {
@@ -26,7 +48,58 @@ function handleFileSelect(event: Event) {
   }
 }
 
+function fileIsTooBig(file: File) {
+  const maxSize = file.type.startsWith("video/")
+    ? MAX_VIDEO_SIZE
+    : MAX_IMAGE_SIZE
+  return file.size > maxSize
+}
+
 const fileUploadId = useId()
+
+type Preview = {
+  type: "image" | "video"
+  url: string
+} | null
+
+const previews = computed<Preview[]>((oldPreviews) => {
+  if (oldPreviews) {
+    cleanupObjectURLs(oldPreviews)
+  }
+
+  return files.value.map((file) => {
+    if (file.type.startsWith("image/")) {
+      return {
+        type: "image",
+        url: URL.createObjectURL(file),
+      }
+    } else if (file.type.startsWith("video/")) {
+      return {
+        type: "video",
+        url: URL.createObjectURL(file),
+      }
+    }
+    return null
+  })
+})
+
+function cleanupObjectURLs(previews: Preview[]) {
+  previews.forEach((preview) => {
+    if (preview?.url) {
+      URL.revokeObjectURL(preview.url)
+    }
+  })
+}
+
+// Cleanup old previews
+onUnmounted(() => {
+  cleanupObjectURLs(previews.value)
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+.preview {
+  max-height: 20rem;
+  max-width: 20rem;
+}
+</style>
