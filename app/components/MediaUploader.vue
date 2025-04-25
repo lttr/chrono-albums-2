0961790c3608820d7bb2b1cdb14c6e35ba25817e
Lukas Trumm <lukas.trumm@gmail.com>
2025-04-25 12:20:26 +0200

    Make the drop zone nicer

--- app/components/MediaUploader.vue
+++ app/components/MediaUploader.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    <section>
-      <label class="upload-label" :for="fileUploadId">Upload files</label>
-      <input
-        :id="fileUploadId"
-        class="upload-input"
-        type="file"
-        multiple
-        :accept="acceptedFileTypes"
-        @change="handleFileSelect"
-      />
+    <section class="p-flow">
+      <!-- eslint-disable-next-line vuejs-accessibility/no-static-element-interactions -->
+      <label
+        class="upload-label p-center p-flow"
+        :class="{ 'is-dragging': isDragging }"
+        :for="fileUploadId"
+        tabindex="0"
+        @keydown.enter="openFileInput"
+        @dragenter="isDragging = true"
+        @dragover="isDragging = true"
+        @dragleave="isDragging = false"
+        @drop="isDragging = false"
+      >
+        <Icon name="uil-cloud-upload" class="icon" />
+        <div class="p-heading-4">Nahraj fotky a videa</div>
+        <div class="upload-hint p-secondary-text-regular">
+          Přetáhni sem soubory nebo klikni na tlačítko. Přijímá soubory ve
+          formátech JPEG, HEIC, MP4 a MOV.
+        </div>
+        <input
+          :id="fileUploadId"
+          ref="fileInput"
+          class="upload-input"
+          type="file"
+          multiple
+          :accept="acceptedFileTypes"
+          @change="handleFileSelect"
+        />
+      </label>
     </section>
     <section>
       <div v-for="(file, index) of files" :key="file.name">
@@ -37,6 +56,10 @@ const acceptedFileTypes = ALLOWED_MIME_TYPES.concat(
 
 const files = ref<File[]>([])
 
+const isDragging = ref(false)
+
+const fileUploadId = useId()
+
 async function handleFileSelect(event: Event) {
   const input = event.target as HTMLInputElement
   const filesAsArray = Array.from(input?.files ?? [])
@@ -61,14 +84,20 @@ async function processFiles(files: File[]) {
   console.log(response)
 }
 
+const fileInput = templateRef<HTMLInputElement>("fileInput")
+
+function openFileInput() {
+  if (fileInput.value) {
+    fileInput.value.click()
+  }
+}
+
 function fileIsTooBig(file: File) {
   const maxSize = file.type.startsWith("video/")
     ? MAX_VIDEO_SIZE
     : MAX_IMAGE_SIZE
   return file.size > maxSize
 }
-
-const fileUploadId = useId()
 
 type Preview = {
   type: "image" | "video"
@@ -114,5 +143,31 @@ onUnmounted(() => {
 .preview {
   max-height: 20rem;
   max-width: 20rem;
+}
+
+.upload-label {
+  padding-block: var(--space-6);
+  padding-inline: var(--space-3);
+  border: var(--border-size-2) dashed var(--text-color-2);
+  border-radius: var(--radius-3);
+  box-shadow: var(--inner-shadow-3);
+
+  &.is-dragging,
+  &:hover {
+    border: var(--border-size-2) dashed var(--text-color-1);
+    box-shadow: var(--inner-shadow-0);
+    background: var(--surface-1);
+  }
+}
+
+.upload-input {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  opacity: 0;
+}
+
+.icon {
+  font-size: 3rem;
 }
 </style>
